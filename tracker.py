1f62c042b7511eed57763354f0e18d687290f0f0
wonnor-pro <wonnor_pro@gmail.com>
2019-08-02 21:46:44 +0800

    tracker with img resized.

--- tracker.py
+++ tracker.py
@@ -1,4 +1,20 @@
 import numpy as np
+import cv2
+
+def drawBoxes(im, boxes, color):
+    x1 = boxes[0]
+    y1 = boxes[1]
+    x2 = boxes[2]
+    y2 = boxes[3]
+    if color == 'r':
+        rgb = (0,0,255)
+    elif color == 'g':
+        rgb = (0,255,0)
+    elif color == 'b':
+        rgb = (255,0,0)
+    cv2.rectangle(im, (int(x1), int(y1)), (int(x2), int(y2)), rgb, 1)
+    print("draw box.")
+    return im
 
 class Tracker:
     """
@@ -45,11 +61,17 @@ class Tracker:
         :param boundingbox:
         :return:
         """
+
+        w = self.window[2] - self.window[0]
+        h = self.window[3] - self.window[1]
+        resize_ratio = min(w/20, h/20)
+        # the smaller ratio is the one we used.
+
         result_bbox = np.zeros(5)
-        result_bbox[0] = int(boundingbox[0]+self.window[0])
-        result_bbox[1] = int(boundingbox[1]+self.window[1])
-        result_bbox[2] = int(boundingbox[2]+self.window[0])
-        result_bbox[3] = int(boundingbox[3]+self.window[1])
+        result_bbox[0] = int(boundingbox[0]*resize_ratio+self.window[0])
+        result_bbox[1] = int(boundingbox[1]*resize_ratio+self.window[1])
+        result_bbox[2] = int(boundingbox[2]*resize_ratio+self.window[0])
+        result_bbox[3] = int(boundingbox[3]*resize_ratio+self.window[1])
         return result_bbox
 
     def __repr__(self):
@@ -61,7 +83,17 @@ class Tracker:
         :param img: new frame
         :return:
         """
-        self.img = img[int(self.window[1]):int(self.window[3]+1),int(self.window[0]):int(self.window[2]+1)]
+        if self.img is None:
+            self.img = img[int(self.window[1]):int(self.window[3]), int(self.window[0]):int(self.window[2])]
+
+        else:
+            self.img = img[int(self.window[1]):int(self.window[3]), int(self.window[0]):int(self.window[2])]
+            img_ratio = self.img.shape[0] / self.img.shape[1]
+            if img_ratio <= 1:
+                self.img = cv2.resize(self.img, (int(20 / img_ratio), 20))
+            if img_ratio > 1:
+                self.img = cv2.resize(self.img, (20, int(20 * img_ratio)))
+
 
     def update_result(self,boundingbox):
         """
@@ -69,6 +101,7 @@ class Tracker:
         :param boundingbox:  The detection result of the cropped image.[x1,y1,x2,y2,score]
         :return:
         """
+
         self.result_bbox = self._offset_bbox(boundingbox)
         self._update_window()
         return self.result_bbox
@@ -83,16 +116,25 @@ class Tracker:
         return self.result_bbox
 
 if __name__ == '__main__':
-    img = np.random.rand(320,240,3)
-    spawn_box = np.array([0,0,200,200,0.9])
+    img = np.zeros((320,240,3))
+    #cv2.imshow('img',img)
+    spawn_box = np.array([40,40,140,90,0.99])
     tracker = Tracker(spawn_box=spawn_box,total_width=320,total_height=240,_id=1)
     print(tracker)
+    window1 = tracker.window
 
-    det_bbox = np.array([30,30,50,50,0.99])
+    det_bbox = np.array([int(15/3.5),int(20/3.5),int(80/3.5),int(100/3.5),0.99])
+    # test offset with the window 15,20,80,100; 3.5 is the resize ratio
+
     tracker.update_result(det_bbox)
     tracker.update_img(img)
     print(tracker)
-    print(tracker.get_result_bbox())
+    img = drawBoxes(img, tracker.get_result_bbox(), 'b')
+
+    cv2.imshow('ground true box', drawBoxes(img, [20+15, 30+20, 20+80, 30+100] , 'g'))
     print(tracker.get_window_img().shape)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
